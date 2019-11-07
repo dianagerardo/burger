7eeea381cc51473f71a537ba76a94eb4aa97dcac
Diana Gerardo <dianagerardo@email.arizona.edu>
2019-11-06 21:55:55 -0700

    Fixing class change in handlebars-block file so that put route worked

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,6 +29,7 @@ router.post("/api/burgers", function(req, res) {
 
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
+  console.log("clicked")
 
   console.log("condition", condition);
 
--- public/assets/js/burger/burgers.js
+++ public/assets/js/burger/burgers.js
@@ -1,6 +1,8 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-    $(".change-devoured").on("click", function(event) {
+    $(".change").on("click", function(event) {
+      
+      
       var id = $(this).data("id");
       var newDevoured = $(this).data("newdevoured");
   
